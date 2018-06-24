93b4895ecee6e91676b8f300c53a91850cf302d7
Karolik <36989581+Karolik@users.noreply.github.com>
2018-06-24 19:44:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var staticCacheName = 'rest-review-v4';
+var staticCacheName = 'rest-review-v5';
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open(staticCacheName).then(function(cache) {
      return cache.addAll([
-       '/skeleton',
+       //'/skeleton',
        '/',
        '/index.html',
        '/index.html?homescreen=1',
@@ -16,7 +16,26 @@ self.addEventListener('install', function(e) {
        '/js/main.js',
        '/js/dbhelper.js',
        '/js/restaurant_info.js',
-       '/img'
+       '/img/1-mission-chinese-food-small.jpg',
+       '/img/1-mission-chinese-food.jpg',
+       '/img/2-emily.jpg',
+       '/img/2-emily-small.jpg',
+       '/img/3-kang-ho-dong-baekjeong-small.jpg',
+       '/img/3-kang-ho-dong-baekjeong.jpg',
+       '/img/4-katzs-delicatessen-small.jpg',
+       '/img/4-katzs-delicatessen.jpg',
+       '/img/5-robertas-pizza-small.jpg',
+       '/img/5-robertas-pizza.jpg',
+       '/img/6-hometown-bbq-small.jpg',
+       '/img/6-hometown-bbq.jpg',
+       '/img/7-superiority-burger-small.jpg',
+       '/img/7-superiority-burger.jpg',
+       '/img/8-the-dutch-small.jpg',
+       '/img/8-the-dutch.jpg',
+       '/img/9-mu-ramen-small.jpg',
+       '/img/9-mu-ramen.jpg',
+       '/img/10-casa-enrique-small.jpg',
+       '/img/10-casa-enrique.jpg'
      ]);
    })
  );
@@ -40,14 +59,14 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
 
-    var requestUrl = new URL(event.request.url);
+    /*var requestUrl = new URL(event.request.url);
     
     if(requestUrl.origin === location.origin) {
       if(requestUrl.pathame ==='/') {
         event.respondWith(caches.match('/skeleton'));
         return;
       }
-    }
+    } */
 
     event.respondWith(
       caches.match(event.request).then(function(response) {
